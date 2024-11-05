012ef99082f19082663f937928278ca4582aba04
asaltcode <elangovanr558@gmail.com>
2024-11-05 18:28:56 +0530

    file updted

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     //apiUrl : "http://localhost:8000"
-     apiUrl : "https://super-market-backend-plum.vercel.app"
+     apiUrl : "https://angular-v8sm.vercel.app"
 };
